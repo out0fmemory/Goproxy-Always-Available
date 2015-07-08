9f7aec3906438d69cb61ba8fba02f9fca2cb770f
Phus Lu <phuslu@hotmail.com>
2015-07-08 21:01:16 +0800

    add *.user.json support to config

--- httpproxy/filters/direct/config.go
+++ httpproxy/filters/direct/config.go
@@ -2,6 +2,8 @@ package direct
 
 import (
 	"encoding/json"
+	"path"
+	"strings"
 
 	"../../../storage"
 )
@@ -29,29 +31,39 @@ type Config struct {
 	}
 }
 
-func NewConfig(uri, path string) (*Config, error) {
+func NewConfig(uri, filename string) (*Config, error) {
 	store, err := storage.OpenURI(uri)
 	if err != nil {
 		return nil, err
 	}
 
-	object, err := store.GetObject(path, -1, -1)
-	if err != nil {
-		return nil, err
-	}
-
-	rc := object.Body()
-	defer rc.Close()
-
-	data, err := storage.ReadJson(rc)
-	if err != nil {
-		return nil, err
-	}
-
 	config := new(Config)
-	err = json.Unmarshal(data, config)
-	if err != nil {
-		return nil, err
+
+	fileext := path.Ext(filename)
+	filename1 := strings.TrimSuffix(filename, fileext) + ".user" + fileext
+
+	for i, name := range []string{filename, filename1} {
+		object, err := store.GetObject(name, -1, -1)
+		if err != nil {
+			if i == 0 {
+				return nil, err
+			} else {
+				continue
+			}
+		}
+
+		rc := object.Body()
+		defer rc.Close()
+
+		data, err := storage.ReadJson(rc)
+		if err != nil {
+			return nil, err
+		}
+
+		err = json.Unmarshal(data, config)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return config, nil
