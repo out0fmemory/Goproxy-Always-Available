b9d21a35af92b3ba8794ab556dc17577666a2378
Phus Lu <phuslu@hotmail.com>
2015-07-09 17:43:35 +0800

    refine gae.go

--- fetchserver/gae/gae.go
+++ fetchserver/gae/gae.go
@@ -4,6 +4,7 @@ package gae
 
 import (
 	"bufio"
+	"bytes"
 	"compress/flate"
 	"encoding/binary"
 	"fmt"
@@ -25,24 +26,33 @@ const (
 	Deadline     = 30 * time.Second
 )
 
-func favicon(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
+func handlerError(rw http.ResponseWriter, html string, code int) {
+	var b bytes.Buffer
+	w, err := flate.NewWriter(&b, flate.BestCompression)
+	if err != nil {
+		rw.Header().Set("Content-Type", "text/plain")
+		rw.WriteHeader(http.StatusBadGateway)
+		io.WriteString(rw, err.Error())
+	}
 
-func robots(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	io.WriteString(w, "User-agent: *\nDisallow: /\n")
-}
-
-func handlerError(w http.ResponseWriter, html string, code int) {
 	fmt.Fprintf(w, "HTTP/1.1 %d\r\n", code)
 	fmt.Fprintf(w, "Content-Type: text/html; charset=utf-8\r\n")
 	fmt.Fprintf(w, "Content-Length: %d\r\n", len(html))
 	io.WriteString(w, "\r\n")
 	io.WriteString(w, html)
+	w.Close()
+
+	b0 := make([]byte, 2)
+	binary.BigEndian.PutUint16(b0, uint16(b.Len()))
+
+	rw.Header().Set("Content-Type", "image/gif")
+	rw.Header().Set("Content-Length", strconv.Itoa(len(b0)+b.Len()))
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(b0)
+	rw.Write(b.Bytes())
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(rw http.ResponseWriter, r *http.Request) {
 	var err error
 	context := appengine.NewContext(r)
 	context.Infof("Hanlde Request %#v\n", r)
@@ -52,15 +62,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		context.Criticalf("binary.Read(&hdrLen) return %v", err)
 	}
 
-	req, err := http.ReadRequest(flate.NewReader(io.LimitedReader{r.Body, int64(hdrLen)}))
+	req, err := http.ReadRequest(bufio.NewReader(flate.NewReader(&io.LimitedReader{r.Body, int64(hdrLen)})))
 	if err != nil {
-		context.Criticalf("http.ReadRequest(%#v) return %#v", rc, err)
+		context.Criticalf("http.ReadRequest(%#v) return %#v", r.Body, err)
 	}
 
 	req.Body = r.Body
 
 	params := make(map[string]string)
-	paramPrefix := "X-Fetch-"
+	paramPrefix := "X-UrlFetch-"
 	for key, values := range r.Header {
 		if strings.HasPrefix(key, paramPrefix) {
 			params[strings.ToLower(key[len(paramPrefix):])] = values[0]
@@ -73,7 +83,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	if Password != "" {
 		if password, ok := params["password"]; !ok || password != Password {
-			handlerError(w, "Wrong Password.", 403)
+			handlerError(rw, "Wrong Password.", 403)
 		}
 	}
 
@@ -89,66 +99,75 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 		errors = append(errors, err)
 		message := err.Error()
-		if strings.Contains(message, "FETCH_ERROR") {
-			context.Warningf("URLFetchServiceError_FETCH_ERROR(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
+		switch {
+		case strings.Contains(message, "FETCH_ERROR"):
+			context.Warningf("FETCH_ERROR(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
 			time.Sleep(time.Second)
 			deadline *= 2
-		} else if strings.Contains(message, "DEADLINE_EXCEEDED") {
-			context.Warningf("URLFetchServiceError_DEADLINE_EXCEEDED(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
+		case strings.Contains(message, "DEADLINE_EXCEEDED"):
+			context.Warningf("DEADLINE_EXCEEDED(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
 			time.Sleep(time.Second)
 			deadline *= 2
-		} else if strings.Contains(message, "INVALID_URL") {
-			handlerError(w, fmt.Sprintf("Invalid URL: %v", err), 501)
+		case strings.Contains(message, "INVALID_URL"):
+			handlerError(rw, fmt.Sprintf("Invalid URL: %v", err), 501)
 			return
-		} else if strings.Contains(message, "RESPONSE_TOO_LARGE") {
-			context.Warningf("URLFetchServiceError_RESPONSE_TOO_LARGE(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
+		case strings.Contains(message, "RESPONSE_TOO_LARGE"):
+			context.Warningf("RESPONSE_TOO_LARGE(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
 			req.Header.Set("Range", fmt.Sprintf("bytes=0-%d", FetchMaxSize))
 			deadline *= 2
-		} else {
+		default:
 			context.Warningf("URLFetchServiceError UNKOWN(type=%T, deadline=%v, url=%v, error=%v)", err, deadline, req.URL, err)
 			time.Sleep(4 * time.Second)
 		}
 	}
 
 	if len(errors) == 2 {
-		handlerError(w, fmt.Sprintf("Go Server Fetch Failed: %v", errors), 502)
+		handlerError(rw, fmt.Sprintf("Go Server Fetch Failed: %v", errors), 502)
 	}
 
-	w.Header().Set("Content-Type", "image/gif")
+	// Fix missing content-length
 	resp.Header.Set("Content-Length", strconv.FormatInt(resp.ContentLength, 10))
 
-	var w1 io.Writer
-	if resp.TransferEncoding == nil && resp.ContentLength <= 1024*1024 {
-		w.Header().Set("X-Content-Encoding", "gzip")
-		gw := gzip.NewWriter(w)
-		defer gw.Close()
-		w1 = gw
-	} else {
-		w1 = w
+	var b bytes.Buffer
+	w, err := flate.NewWriter(&b, flate.BestCompression)
+	if err != nil {
+		handlerError(rw, fmt.Sprintf("Go Server Fetch Failed: %v", w), 502)
 	}
 
-	fmt.Fprintf(w1, "%s %s\r\n", resp.Proto, resp.Status)
-	for key, values := range resp.Header {
-		for _, value := range values {
-			fmt.Fprintf(w1, "%s: %s\r\n", key, value)
-		}
-	}
-	io.WriteString(w1, "\r\n")
-	io.Copy(w1, resp.Body)
+	resp.Header.Write(w)
+	w.Close()
+
+	b0 := make([]byte, 2)
+	binary.BigEndian.PutUint16(b0, uint16(b.Len()))
+
+	rw.Header().Set("Content-Type", "image/gif")
+	rw.Header().Set("Content-Length", strconv.FormatInt(int64(len(b0)+b.Len())+resp.ContentLength, 10))
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(b0)
+	rw.Write(b.Bytes())
+	io.Copy(rw, resp.Body)
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
+func favicon(rw http.ResponseWriter, r *http.Request) {
+	rw.WriteHeader(http.StatusOK)
+}
+
+func robots(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	io.WriteString(rw, "User-agent: *\nDisallow: /\n")
+}
+
+func root(rw http.ResponseWriter, r *http.Request) {
 	context := appengine.NewContext(r)
 	version, _ := strconv.ParseInt(strings.Split(appengine.VersionID(context), ".")[1], 10, 64)
 	ctime := time.Unix(version/(1<<28)+8*3600, 0).Format(time.RFC3339)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintf(w, "GoAgent go server %s works, deployed at %s\n", Version, ctime)
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(rw, "GoProxy server %s works, deployed at %s\n", Version, ctime)
 }
 
 func init() {
+	http.HandleFunc("/_gh/", handler)
 	http.HandleFunc("/favicon.ico", favicon)
 	http.HandleFunc("/robots.txt", robots)
-	http.HandleFunc("/_gh/", handler)
-	http.HandleFunc("/_gh/gzip", handler)
 	http.HandleFunc("/", root)
 }
