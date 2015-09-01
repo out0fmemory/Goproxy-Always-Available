9a902c65b99251ba20aed4de0a7233dd8abc1949
Phus Lu <phuslu@hotmail.com>
2015-09-02 00:18:21 +0800

    comment CONNECT for vps.go

--- assets/scripts/release.sh
+++ assets/scripts/release.sh
@@ -3,10 +3,13 @@
 GOROOT=$HOME/go1.5
 GOPATH=$HOME/go1.5/gopath
 PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+SOURCEDIR=${GOPATH}/src/github.com/phuslu/goproxy
+DISTDIR=${SOURCEDIR}/dist
 
-cd ${GOPATH}/src/github.com/phuslu/goproxy
+cd ${SOURCEDIR}
 git clean -df
 git reset --hard
+mkdir -p ${DISTDIR}
 
 git branch -D bak
 git checkout -b bak
@@ -18,21 +21,22 @@ REV=`git rev-list HEAD | wc -l | xargs`
 NOTE=`git log --oneline | head -1`
 REMOTE=`git remote -v | head -1 | awk '{print $2}'`
 
-cd ${GOPATH}/src/github.com/phuslu/goproxy/goproxy
-mkdir -p dist
+cd ${SOURCEDIR}/fetchserver/vps
+GOOS=linux GOARCH=amd64 make && mv build/dist/govps* ${DISTDIR}/ && make clean
 
-GOOS=windows GOARCH=386 make && mv build/dist/goproxy* dist/ && make clean
-GOOS=windows GOARCH=amd64 make && mv build/dist/goproxy* dist/ && make clean
-GOOS=linux GOARCH=amd64 make && mv build/dist/goproxy* dist/ && make clean
-GOOS=linux GOARCH=386 make && mv build/dist/goproxy* dist/ && make clean
-GOOS=linux GOARCH=arm make && mv build/dist/goproxy* dist/ && make clean
-GOOS=darwin GOARCH=amd64 make && mv build/dist/goproxy* dist/ && make clean
-GOOS=darwin GOARCH=386 make && mv build/dist/goproxy* dist/ && make clean
+cd ${SOURCEDIR}/goproxy
+GOOS=windows GOARCH=386 make && mv build/dist/goproxy* ${DISTDIR}/ && make clean
+GOOS=windows GOARCH=amd64 make && mv build/dist/goproxy* ${DISTDIR}/ && make clean
+GOOS=linux GOARCH=amd64 make && mv build/dist/goproxy* ${DISTDIR}/ && make clean
+GOOS=linux GOARCH=386 make && mv build/dist/goproxy* ${DISTDIR}/ && make clean
+GOOS=linux GOARCH=arm make && mv build/dist/goproxy* ${DISTDIR}/ && make clean
+GOOS=darwin GOARCH=amd64 make && mv build/dist/goproxy* ${DISTDIR}/ && make clean
+GOOS=darwin GOARCH=386 make && mv build/dist/goproxy* ${DISTDIR}/ && make clean
 
 export GITHUB_TOKEN=`cat ~/GITHUB_TOKEN`
 
 github-release delete --user phuslu --repo goproxy --tag goproxy
 github-release release --user phuslu --repo goproxy --tag goproxy --name "goproxy r${REV}" --description "r${REV}: ${NOTE}"
-for f in `ls dist`; do
-    github-release -v upload --user phuslu --repo goproxy --tag goproxy --name $f --file dist/$f
+for f in `ls ${DISTDIR}`; do
+    github-release -v upload --user phuslu --repo goproxy --tag goproxy --name $f --file ${DISTDIR}/$f
 done
--- httpproxy/filters/vps/vps.go
+++ httpproxy/filters/vps/vps.go
@@ -1,7 +1,7 @@
 package vps
 
 import (
-	"fmt"
+	// "fmt"
 	"math/rand"
 	"net/http"
 	"net/url"
@@ -110,40 +110,40 @@ func (f *Filter) RoundTrip(ctx *filters.Context, req *http.Request) (*filters.Co
 
 	fetchServer := f.FetchServers[i]
 
-	if req.Method == "CONNECT" {
-		rconn, err := fetchServer.Transport.Connect(req)
-		if err != nil {
-			return ctx, nil, err
-		}
-		defer rconn.Close()
+	// if req.Method == "CONNECT" {
+	// 	rconn, err := fetchServer.Transport.Connect(req)
+	// 	if err != nil {
+	// 		return ctx, nil, err
+	// 	}
+	// 	defer rconn.Close()
 
-		rw := ctx.GetResponseWriter()
+	// 	rw := ctx.GetResponseWriter()
 
-		hijacker, ok := rw.(http.Hijacker)
-		if !ok {
-			return ctx, nil, fmt.Errorf("http.ResponseWriter(%#v) does not implments http.Hijacker", rw)
-		}
+	// 	hijacker, ok := rw.(http.Hijacker)
+	// 	if !ok {
+	// 		return ctx, nil, fmt.Errorf("http.ResponseWriter(%#v) does not implments http.Hijacker", rw)
+	// 	}
 
-		flusher, ok := rw.(http.Flusher)
-		if !ok {
-			return ctx, nil, fmt.Errorf("http.ResponseWriter(%#v) does not implments http.Flusher", rw)
-		}
+	// 	flusher, ok := rw.(http.Flusher)
+	// 	if !ok {
+	// 		return ctx, nil, fmt.Errorf("http.ResponseWriter(%#v) does not implments http.Flusher", rw)
+	// 	}
 
-		rw.WriteHeader(http.StatusOK)
-		flusher.Flush()
+	// 	rw.WriteHeader(http.StatusOK)
+	// 	flusher.Flush()
 
-		lconn, _, err := hijacker.Hijack()
-		if err != nil {
-			return ctx, nil, fmt.Errorf("%#v.Hijack() error: %v", hijacker, err)
-		}
-		defer lconn.Close()
+	// 	lconn, _, err := hijacker.Hijack()
+	// 	if err != nil {
+	// 		return ctx, nil, fmt.Errorf("%#v.Hijack() error: %v", hijacker, err)
+	// 	}
+	// 	defer lconn.Close()
 
-		go httpproxy.IoCopy(rconn, lconn)
-		httpproxy.IoCopy(lconn, rconn)
+	// 	go httpproxy.IoCopy(rconn, lconn)
+	// 	httpproxy.IoCopy(lconn, rconn)
 
-		ctx.SetHijacked(true)
-		return ctx, nil, nil
-	}
+	// 	ctx.SetHijacked(true)
+	// 	return ctx, nil, nil
+	// }
 	resp, err := fetchServer.RoundTrip(req)
 	if err != nil {
 		return ctx, nil, err
