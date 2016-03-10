5683df57bf2149a68f0944b801459b224c9af6dc
Phus Lu <phuslu@hotmail.com>
2016-03-11 00:20:59 +0800

    refact transport/php

--- httpproxy/transport/php/php.go
+++ httpproxy/transport/php/php.go
@@ -2,49 +2,18 @@ package php
 
 import (
 	"fmt"
-	"math/rand"
 	"net/http"
-	"path"
-	"strings"
 
 	"github.com/golang/glog"
 )
 
 type Transport struct {
 	http.RoundTripper
-	Servers []Server
+	Server
 }
 
 func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
-	i := 0
-	switch path.Ext(req.URL.Path) {
-	case ".jpg", ".png", ".webp", ".bmp", ".gif", ".flv", ".mp4":
-		i = rand.Intn(len(t.Servers))
-	case "":
-		name := path.Base(req.URL.Path)
-		if strings.Contains(name, "play") ||
-			strings.Contains(name, "video") {
-			i = rand.Intn(len(t.Servers))
-		}
-	default:
-		if strings.Contains(req.URL.Host, "img.") ||
-			strings.Contains(req.URL.Host, "cache.") ||
-			strings.Contains(req.URL.Host, "video.") ||
-			strings.Contains(req.URL.Host, "static.") ||
-			strings.HasPrefix(req.URL.Host, "img") ||
-			strings.HasPrefix(req.URL.Path, "/static") ||
-			strings.HasPrefix(req.URL.Path, "/asset") ||
-			strings.Contains(req.URL.Path, "min.js") ||
-			strings.Contains(req.URL.Path, "static") ||
-			strings.Contains(req.URL.Path, "asset") ||
-			strings.Contains(req.URL.Path, "/cache/") {
-			i = rand.Intn(len(t.Servers))
-		}
-	}
-
-	s := t.Servers[i]
-
-	req1, err := s.encodeRequest(req)
+	req1, err := t.Server.encodeRequest(req)
 	if err != nil {
 		return nil, fmt.Errorf("PHP encodeRequest: %s", err.Error())
 	}
@@ -55,6 +24,6 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	} else {
 		glog.Infof("%s \"PHP %s %s %s\" %d %s", req.RemoteAddr, req.Method, req.URL.String(), req.Proto, res.StatusCode, res.Header.Get("Content-Length"))
 	}
-	resp, err := s.decodeResponse(res)
+	resp, err := t.Server.decodeResponse(res)
 	return resp, err
 }
