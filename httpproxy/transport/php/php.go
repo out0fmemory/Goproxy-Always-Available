ebcde1588d7d523e803b696c5f9badfb9541d236
Phus Lu <phuslu@hotmail.com>
2016-03-15 01:09:03 +0800

    dedup logs

--- httpproxy/transport/php/php.go
+++ httpproxy/transport/php/php.go
@@ -3,8 +3,6 @@ package php
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/golang/glog"
 )
 
 type Transport struct {
@@ -21,9 +19,8 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	res, err := t.RoundTripper.RoundTrip(req1)
 	if err != nil {
 		return nil, err
-	} else {
-		glog.Infof("%s \"PHP %s %s %s\" %d %s", req.RemoteAddr, req.Method, req.URL.String(), req.Proto, res.StatusCode, res.Header.Get("Content-Length"))
 	}
+
 	resp, err := t.Server.decodeResponse(res)
 	return resp, err
 }
