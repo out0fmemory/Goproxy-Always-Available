3c7241b8bded04516f48ba82dbf2bc3eee4a404f
Phus Lu <phuslu@hotmail.com>
2016-03-23 23:16:35 +0800

    improve release.sh matching

--- assets/scripts/release.sh
+++ assets/scripts/release.sh
@@ -2,18 +2,15 @@
 
 set -x
 
-export GITHUB_USER=phuslu
+export GITHUB_USER=${GITHUB_USER:-phuslu}
 export GITHUB_REPO=goproxy
 export GITHUB_CI_REPO=goproxy-ci
 export GITHUB_TAG=${GITHUB_TAG}
-export GITHUB_TOKEN=${GITHUB_TOKEN}
+export GITHUB_TOKEN=${GITHUB_TOKEN:-$(cat ~/GITHUB_TOKEN)}
 export GITHUB_CI_REPO_RELEASE_INFO_TXT=github-${GITHUB_USER}-${GITHUB_CI_REPO}-${GITHUB_TAG}-release-info.txt
 if [ -z "$GITHUB_TOKEN" ]; then
-	export GITHUB_TOKEN=`cat ~/GITHUB_TOKEN`
-	if [ -z "$GITHUB_TOKEN" ]; then
-		echo Please set GITHUB_TOKEN envar
-		exit 1
-	fi
+	echo Please set GITHUB_TOKEN envar
+	exit 1
 fi
 if [ -z "$GITHUB_TAG" ]; then
 	echo Please set GITHUB_TAG envar
@@ -29,7 +26,7 @@ export PATH=${WORKING_DIR}/bin/linux/amd64:$PATH
 github-release info -u ${GITHUB_USER} -r ${GITHUB_CI_REPO} -t ${GITHUB_TAG} > ${GITHUB_CI_REPO_RELEASE_INFO_TXT}
 export RELEASE_NAME=`cat ${GITHUB_CI_REPO_RELEASE_INFO_TXT} | grep -oP "name: '\K.+?'," | sed 's/..$//'`
 export RELEASE_NOTE=`cat ${GITHUB_CI_REPO_RELEASE_INFO_TXT} | grep -oP "description: '\K.+?'," | sed 's/..$//'`
-export RELEASE_FILES=`cat ${GITHUB_CI_REPO_RELEASE_INFO_TXT} | grep -oP 'goproxy_.+\.(7z|zip|gz|bz2|tar)'`
+export RELEASE_FILES=`cat ${GITHUB_CI_REPO_RELEASE_INFO_TXT} | grep -oP 'artifact: \K\S+\.(7z|zip|gz|bz2|tar)'`
 
 for FILE in ${RELEASE_FILES}; do
     github-release download --user ${GITHUB_USER} --repo ${GITHUB_CI_REPO} --tag ${GITHUB_TAG} --name "$FILE"
