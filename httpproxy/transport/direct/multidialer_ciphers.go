3d07128136eabb5ef6557fd1d3e7b308fd9609da
Phus Lu <phuslu@hotmail.com>
2016-03-27 11:23:41 +0800

    add ClientSessionCache to mutlidialer TLSConfig

--- httpproxy/transport/direct/multidialer_ciphers.go
+++ httpproxy/transport/direct/multidialer_ciphers.go
@@ -11,6 +11,7 @@ var (
 		MinVersion:         tls.VersionTLS12,
 		InsecureSkipVerify: true,
 		ServerName:         "www.bing.com",
+		ClientSessionCache: tls.NewLRUClientSessionCache(1000),
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
 			// tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
