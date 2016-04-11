f44dbf33718deff36401e76410657e15aeb037f0
Phus Lu <phuslu@hotmail.com>
2016-04-11 18:53:01 +0800

    refactor CI script

--- assets/scripts/ci.sh
+++ assets/scripts/ci.sh
@@ -4,7 +4,7 @@ export GITHUB_USER=${GITHUB_USER:-phuslu}
 export GITHUB_REPO=${GITHUB_REPO:-goproxy}
 export GITHUB_CI_REPO=${GITHUB_CI_REPO:-goproxy-ci}
 export GITHUB_COMMIT_ID=${TRAVIS_COMMIT:-${COMMIT_ID:-master}}
-export WORKING_DIR=$HOME/tmp.${RANDOM:-$$}.${GITHUB_REPO}
+export WORKING_DIR=$HOME/tmp.$(date "+%Y%m%d%H%M%S").${RANDOM:-$$}.${GITHUB_REPO}
 export GOROOT_BOOTSTRAP=${WORKING_DIR}/go1.6
 export GOROOT=${WORKING_DIR}/go
 export GOPATH=${WORKING_DIR}/gopath
@@ -15,59 +15,102 @@ if [ ${#GITHUB_TOKEN} -eq 0 ]; then
 	exit 1
 fi
 
-git config --global user.name ${GITHUB_USER}
-git config --global user.email "${GITHUB_USER}@noreply.github.com"
+function init_github() {
+	mkdir -p ${WORKING_DIR} && cd ${WORKING_DIR}
 
-grep -q 'machine github.com' ~/.netrc || echo "machine github.com login $GITHUB_USER password $GITHUB_TOKEN" >>~/.netrc
+	git config --global user.name ${GITHUB_USER}
+	git config --global user.email "${GITHUB_USER}@noreply.github.com"
+	# grep -q 'machine github.com' ~/.netrc || echo "machine github.com login $GITHUB_USER password $GITHUB_TOKEN" >>~/.netrc
 
-mkdir -p ${WORKING_DIR}
-cd ${WORKING_DIR}
+	local GITHUB_RELEASE_BINARY_URL=https://github.com/aktau/github-release/releases/download/v0.6.2/linux-amd64-github-release.tar.bz2
+	local GITHUB_RELEASE_BINARY_PATH=$(pwd)/$(curl -L ${GITHUB_RELEASE_BINARY_URL} | tar xjpv | head -1)
+	export PATH=$PATH:${GITHUB_RELEASE_BINARY_PATH%/*}
+}
 
-curl -k https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz | tar xz
-mv go go1.6
+function build_go() {
+	mkdir -p ${WORKING_DIR} && cd ${WORKING_DIR}
 
-git clone https://github.com/phuslu/go
-(cd go && git remote add -f upstream https://github.com/golang/go && git rebase upstream/master && git push -f origin master)
-(cd go/src/ && BUILD_GO_TAG_BACK_STEPS=~3 bash ./make.bash)
+	curl -k https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz | tar xz
+	mv go go1.6
 
-echo && cat /etc/issue && uname -a && echo && go version && echo && go env && echo && env | grep -v GITHUB_TOKEN
+	git clone https://github.com/phuslu/go
+	(cd go &&\
+		git remote add -f upstream https://github.com/golang/go &&\
+		git rebase upstream/master &&\
+		git push -f origin master \
+	)
+	(cd go/src/ &&\
+		BUILD_GO_TAG_BACK_STEPS=~3 bash ./make.bash \
+	)
 
-git clone --branch "master" https://github.com/${GITHUB_USER}/${GITHUB_REPO} ${GITHUB_REPO}
-git clone --branch "master" https://github.com/${GITHUB_USER}/${GITHUB_CI_REPO} ${GITHUB_CI_REPO}
+	echo '======================================'
+	cat /etc/issue
+	uname -a
+	echo
+	go version
+	go env
+	echo
+	env | grep -v GITHUB_TOKEN
+	echo '======================================'
+}
 
-curl -L https://github.com/aktau/github-release/releases/download/v0.6.2/linux-amd64-github-release.tar.bz2 | tar xjpv | xargs -n1 -i mv -f {} $GOROOT/bin/
+function build_goproxy() {
+	mkdir -p ${WORKING_DIR} && cd ${WORKING_DIR}
 
-cd ${GITHUB_REPO}
-git checkout -f ${GITHUB_COMMIT_ID}
+	git clone --branch "master" https://github.com/${GITHUB_USER}/${GITHUB_REPO} ${GITHUB_REPO}
 
-export RELEASE=$(git rev-list HEAD| wc -l |xargs)
-export RELEASE_DESCRIPTION=$(git log -1 --oneline --format="r${RELEASE}: [\`%h\`](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/commit/%h) %s")
-if [ -n "${TRAVIS_BUILD_ID}" ]; then
-	export RELEASE_DESCRIPTION=$(echo ${RELEASE_DESCRIPTION} | sed -E "s#^(r[0-9]+)#[\1](https://travis-ci.org/${GITHUB_USER}/${GITHUB_REPO}/builds/${TRAVIS_BUILD_ID})#g")
-fi
+	cd ${GITHUB_REPO}
+	git checkout -f ${GITHUB_COMMIT_ID}
 
-mkdir ${WORKING_DIR}/r${RELEASE}
+	export RELEASE=$(git rev-list HEAD| wc -l |xargs)
+	export RELEASE_DESCRIPTION=$(git log -1 --oneline --format="r${RELEASE}: [\`%h\`](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/commit/%h) %s")
+	if [ -n "${TRAVIS_BUILD_ID}" ]; then
+		export RELEASE_DESCRIPTION=$(echo ${RELEASE_DESCRIPTION} | sed -E "s#^(r[0-9]+)#[\1](https://travis-ci.org/${GITHUB_USER}/${GITHUB_REPO}/builds/${TRAVIS_BUILD_ID})#g")
+	fi
 
-awk 'match($1, /"((github\.com|golang\.org|gopkg\.in)\/.+)"/) {if (!seen[$1]++) {gsub("\"", "", $1); print $1}}' $(find . -name "*.go") | xargs -n1 -i go get -v {}
+	mkdir ${WORKING_DIR}/r${RELEASE}
 
-for OSARCH in linux/amd64 linux/386 linux/arm linux/arm64 linux/mips64 linux/mips64le darwin/amd64 darwin/386 windows/amd64 windows/386; do
-	make GOOS=${OSARCH%/*} GOARCH=${OSARCH#*/}
-	cp -r build/dist/* ${WORKING_DIR}/r${RELEASE}
-	make clean
-done
+	awk 'match($1, /"((github\.com|golang\.org|gopkg\.in)\/.+)"/) {if (!seen[$1]++) {gsub("\"", "", $1); print $1}}' $(find . -name "*.go") | xargs -n1 -i go get -v {}
 
-ls -lht ${WORKING_DIR}/r${RELEASE}/*
+	for OSARCH in linux/amd64 linux/386 linux/arm linux/arm64 linux/mips64 linux/mips64le darwin/amd64 darwin/386 windows/amd64 windows/386; do
+		make GOOS=${OSARCH%/*} GOARCH=${OSARCH#*/}
+		cp -r build/dist/* ${WORKING_DIR}/r${RELEASE}
+		make clean
+	done
 
-cd ${WORKING_DIR}/${GITHUB_CI_REPO}/
-git commit --allow-empty -m "release"
-git tag -d r${RELEASE} || true
-git tag r${RELEASE}
-git push -f origin r${RELEASE}
+	ls -lht ${WORKING_DIR}/r${RELEASE}/*
+}
 
-cd ${WORKING_DIR}/r${RELEASE}/
-github-release delete --user ${GITHUB_USER} --repo ${GITHUB_CI_REPO} --tag r${RELEASE} >/dev/null 2>&1 || true
-sleep 1
-github-release release --pre-release --user ${GITHUB_USER} --repo ${GITHUB_CI_REPO} --tag r${RELEASE} --name "${GITHUB_REPO} r${RELEASE}" --description "${RELEASE_DESCRIPTION}"
+function release_goproxy_ci() {
+	mkdir -p ${WORKING_DIR} && cd ${WORKING_DIR}
 
-for FILE in *; do github-release upload --user ${GITHUB_USER} --repo ${GITHUB_CI_REPO} --tag r${RELEASE} --name ${FILE} --file ${FILE}; done
-ls -lht && cd && rm -rf $HOME/tmp.*.${GITHUB_REPO}
+	git clone --branch "master" https://github.com/${GITHUB_USER}/${GITHUB_CI_REPO} ${GITHUB_CI_REPO}
+	cd ${GITHUB_CI_REPO}
+
+	git commit --allow-empty -m "release"
+	git tag -d r${RELEASE} || true
+	git tag r${RELEASE}
+	git push -f origin r${RELEASE}
+
+	cd ${WORKING_DIR}/r${RELEASE}/
+	github-release delete --user ${GITHUB_USER} --repo ${GITHUB_CI_REPO} --tag r${RELEASE} >/dev/null 2>&1 || true
+	sleep 1
+	github-release release --pre-release --user ${GITHUB_USER} --repo ${GITHUB_CI_REPO} --tag r${RELEASE} --name "${GITHUB_REPO} r${RELEASE}" --description "${RELEASE_DESCRIPTION}"
+
+	for FILE in *; do
+		github-release upload --user ${GITHUB_USER} --repo ${GITHUB_CI_REPO} --tag r${RELEASE} --name ${FILE} --file ${FILE}
+	done
+}
+
+function clean() {
+	cd ${WORKING_DIR}/r${RELEASE}/
+	ls -lht
+	cd
+	rm -rf $HOME/tmp.*.${GITHUB_REPO}
+}
+
+init_github
+build_go
+build_goproxy
+release_goproxy_ci
+clean
