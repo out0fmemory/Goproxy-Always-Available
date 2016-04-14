4297895530bcf31286a0ff779ab6378e87a950b7
Phus Lu <phuslu@hotmail.com>
2016-04-15 01:16:13 +0800

    rewise start banner

--- main.go
+++ main.go
@@ -31,24 +31,29 @@ func main() {
 
 	gover := strings.Split(strings.Replace(runtime.Version(), "devel +", "devel+", 1), " ")[0]
 
+	fmt.Fprintf(os.Stderr, `------------------------------------------------------
+GoProxy Version    : %s (go/%s %s/%s)`,
+		version, gover, runtime.GOOS, runtime.GOARCH)
 	for profile, config := range httpproxy.Config {
 		if !config.Enabled {
 			continue
 		}
-		fmt.Fprintf(os.Stderr, `------------------------------------------------------
-GoProxy Version    : %s (go/%s %s/%s)
+		fmt.Fprintf(os.Stderr, `
 GoProxy Profile    : %s
 Listen Address     : %s
-Enabled Filters    : %v
-Pac Server         : http://%s/proxy.pac
-------------------------------------------------------
-`, version, gover, runtime.GOOS, runtime.GOARCH,
-			profile,
+Enabled Filters    : %v`, profile,
 			config.Address,
-			fmt.Sprintf("%s|%s|%s", strings.Join(config.RequestFilters, ","), strings.Join(config.RoundTripFilters, ","), strings.Join(config.ResponseFilters, ",")),
-			config.Address)
+			fmt.Sprintf("%s|%s|%s", strings.Join(config.RequestFilters, ","), strings.Join(config.RoundTripFilters, ","), strings.Join(config.ResponseFilters, ",")))
+		for _, fn := range config.RoundTripFilters {
+			if fn == "autoproxy" {
+				fmt.Fprintf(os.Stderr, `
+Pac Server         : http://%s/proxy.pac`,
+					config.Address)
+			}
+		}
 		go httpproxy.ServeProfile(profile)
 	}
+	fmt.Fprintf(os.Stderr, "\n------------------------------------------------------\n")
 
 	if runtime.GOOS == "windows" {
 		helpers.SetConsoleTitle(fmt.Sprintf("GoProxy %s (go/%s)", version, gover))
