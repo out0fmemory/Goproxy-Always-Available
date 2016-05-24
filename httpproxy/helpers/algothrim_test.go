a6ab0a28baaa8582eec49e328ed123c08de2c809
Phus Lu <phuslu@hotmail.com>
2016-05-24 22:23:32 +0800

    add IsBinary helper

---
+++ httpproxy/helpers/algothrim_test.go
@@ -0,0 +1,19 @@
+package helpers
+
+import (
+	"testing"
+)
+
+func TestIsBinary1(t *testing.T) {
+	data := []byte("\xed\xbdyW\x1b")
+	if !IsBinary(data) {
+		t.Errorf("data=%s must be binary", data)
+	}
+}
+
+func TestIsBinary2(t *testing.T) {
+	data := []byte("hello world!")
+	if IsBinary(data) {
+		t.Errorf("data=%s must not be binary", data)
+	}
+}
