6bbd2ca90bb794030bcf3e591d6b9ed2f1f062d3
Phus Lu <phuslu@hotmail.com>
2016-06-01 21:46:00 +0800

    revise FragmentPipe

--- httpproxy/filters/autorange/pipe2.go
+++ httpproxy/filters/autorange/pipe2.go
@@ -6,78 +6,95 @@ import (
 	"io"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 type fragment struct {
 	data  []byte
-	pos   int
+	pos   int64
 	index int
 }
 
-type fragmentQueue []*fragment
+type fragmentQueue struct {
+	q  []fragment
+	mu sync.Mutex
+}
 
 func (q fragmentQueue) Len() int {
-	return len(q)
+	return len(q.q)
 }
 
 func (q fragmentQueue) Less(i, j int) bool {
-	return q[i].pos < q[j].pos
+	return q.q[i].pos < q.q[j].pos
 }
 
 func (q fragmentQueue) Swap(i, j int) {
-	q[i], q[j] = q[j], q[i]
-	q[i].index = i
-	q[j].index = j
+	q.q[i], q.q[j] = q.q[j], q.q[i]
+	q.q[i].index = i
+	q.q[j].index = j
 }
 
 func (q *fragmentQueue) Push(x interface{}) {
-	n := len(*q)
-	item := x.(*fragment)
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	n := len(q.q)
+	item := x.(fragment)
 	item.index = n
-	*q = append(*q, item)
+	q.q = append(q.q, item)
 }
 
 func (q *fragmentQueue) Pop() interface{} {
-	old := *q
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	old := q.q
 	n := len(old)
 	item := old[n-1]
 	item.index = -1 // for safety
-	*q = old[0 : n-1]
+	q.q = old[0 : n-1]
 	return item
-}
-
-func (q *fragmentQueue) Top() *fragment {
-	return (*q)[len(*q)-1]
 }
 
 type FragmentPipe struct {
 	l    int64
-	mu   sync.Mutex
 	err  error
-	pos  int
-	size int
+	pos  int64
+	size int64
 	q    fragmentQueue
+	c    *sync.Cond
+}
+
+func NewFragmentPipe(size int64) *FragmentPipe {
+	return &FragmentPipe{
+		size: size,
+		q: fragmentQueue{
+			q: []fragment{},
+		},
+		c: sync.NewCond(&sync.Mutex{}),
+	}
 }
 
 func (p *FragmentPipe) Len() int64 {
 	return atomic.LoadInt64(&p.l)
 }
 
-func (p *FragmentPipe) Write(data []byte, pos int) (int, error) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	heap.Push(&p.q, &fragment{data, pos, -1})
+func (p *FragmentPipe) Write(data []byte, pos int64) (int, error) {
+	if pos == p.pos {
+		defer p.c.Signal()
+	}
+	heap.Push(&p.q, fragment{data, pos, -1})
 	atomic.AddInt64(&p.l, int64(len(data)))
 	return len(data), nil
 }
 
 func (p *FragmentPipe) Close() error {
+	defer p.c.Signal()
 	return p.CloseWithError(io.EOF)
 }
 
 func (p *FragmentPipe) CloseWithError(err error) error {
-	p.mu.Lock()
-	defer p.mu.Unlock()
+	defer p.c.Signal()
 	if err == nil {
 		err = io.EOF
 	}
@@ -86,9 +103,6 @@ func (p *FragmentPipe) CloseWithError(err error) error {
 }
 
 func (p *FragmentPipe) Read(data []byte) (int, error) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-
 	if p.err != nil {
 		return 0, p.err
 	}
@@ -98,7 +112,7 @@ func (p *FragmentPipe) Read(data []byte) (int, error) {
 		return 0, io.EOF
 	}
 
-	top := p.q.Top()
+	top := heap.Pop(&p.q).(fragment)
 	if p.pos > top.pos {
 		return 0, fmt.Errorf("%T.pos=%d is larger then %d", p, p.pos, top.pos)
 	} else if p.pos < top.pos {
@@ -107,11 +121,42 @@ func (p *FragmentPipe) Read(data []byte) (int, error) {
 		n := copy(data, top.data)
 		if n < len(top.data) {
 			top.data = top.data[n:]
-			top.pos += n
-		} else {
-			heap.Pop(&p.q)
+			top.pos += int64(n)
+			heap.Push(&p.q, top)
 		}
 		atomic.AddInt64(&p.l, -int64(n))
 		return n, nil
 	}
+}
+
+func (p *FragmentPipe) WriteTo(w io.Writer) (int64, error) {
+	var n int64
+	for n < p.size {
+		if p.err != nil {
+			return 0, p.err
+		}
+		if p.q.Len() == 0 {
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		top := heap.Pop(&p.q).(fragment)
+		if top.pos < n {
+			return 0, fmt.Errorf("%T.pos=%d is larger then %d", top, top.pos, n)
+		} else if top.pos > n {
+			time.Sleep(100 * time.Millisecond)
+			continue
+		} else {
+			n1, err := w.Write(top.data)
+			n += int64(n1)
+			if err != nil {
+				return n, err
+			}
+			if n1 < len(top.data) {
+				top.data = top.data[n1:]
+				top.pos += int64(n1)
+				heap.Push(&p.q, top)
+			}
+		}
+	}
+	return n, nil
 }
