c55173471dd32578c734ca28f2f59f0a36f86475
Phus Lu <phuslu@hotmail.com>
2016-06-04 23:09:10 +0800

    add more tests

--- httpproxy/helpers/fragment_test.go
+++ httpproxy/helpers/fragment_test.go
@@ -77,3 +77,28 @@ func TestFragmentPipe5(t *testing.T) {
 		t.Logf("ioutil.ReadAll(%T) return:err=%v", p, err)
 	}
 }
+
+func TestFragmentPipe6(t *testing.T) {
+	p := NewFragmentPipe(12)
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		p.WriteString("foo", 0)
+	}()
+	go func() {
+		time.Sleep(0 * time.Millisecond)
+		p.WriteString("bar", 3)
+	}()
+	go func() {
+		time.Sleep(40 * time.Millisecond)
+		p.WriteString("foo", 6)
+	}()
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		p.WriteString("bar", 9)
+	}()
+	data, err := ioutil.ReadAll(p)
+	if err != nil {
+		t.Errorf("ioutil.ReadAll(%T) error: %v", p, err)
+	}
+	t.Logf("ioutil.ReadAll(%T) return: %#v", p, string(data))
+}
