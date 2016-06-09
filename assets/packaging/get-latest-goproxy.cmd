e5e09b8db75c8a7b8c015ba4a232965e9d56f541
Phus Lu <phuslu@hotmail.com>
2016-06-09 20:00:45 +0800

    set proxy for get-latest-goproxy.cmd

--- assets/packaging/get-latest-goproxy.cmd
+++ assets/packaging/get-latest-goproxy.cmd
@@ -5,16 +5,19 @@ setlocal enabledelayedexpansion
 
 cd /d "%~dp0"
 
-echo. >"%TEMP%\gdownload.vbs"
-echo Set Http = CreateObject("WinHttp.WinHttpRequest.5.1") >>"%TEMP%\gdownload.vbs"
-echo Set Stream = CreateObject("Adodb.Stream") >>"%TEMP%\gdownload.vbs"
-echo Http.SetTimeouts 30*1000, 30*1000, 30*1000, 120*1000  >>"%TEMP%\gdownload.vbs"
-echo Http.Open "GET", WScript.Arguments.Item(0), False >>"%TEMP%\gdownload.vbs"
-echo Http.Send >>"%TEMP%\gdownload.vbs"
-echo Stream.Type = 1 >>"%TEMP%\gdownload.vbs"
-echo Stream.Open >>"%TEMP%\gdownload.vbs"
-echo Stream.Write Http.ResponseBody >>"%TEMP%\gdownload.vbs"
-echo Stream.SaveToFile WScript.Arguments.Item(1), 2 >>"%TEMP%\gdownload.vbs"
+echo. >~gdownload.vbs
+echo Set Http = CreateObject("WinHttp.WinHttpRequest.5.1") >>~gdownload.vbs
+echo Set Stream = CreateObject("Adodb.Stream") >>~gdownload.vbs
+echo Http.SetTimeouts 30*1000, 30*1000, 30*1000, 120*1000  >>~gdownload.vbs
+netstat -an| findstr LISTENING | findstr ":8087" && (
+    echo Http.SetProxy 2, "127.0.0.1:8087", "" >>~gdownload.vbs
+)
+echo Http.Open "GET", WScript.Arguments.Item(0), False >>~gdownload.vbs
+echo Http.Send >>~gdownload.vbs
+echo Stream.Type = 1 >>~gdownload.vbs
+echo Stream.Open >>~gdownload.vbs
+echo Stream.Write Http.ResponseBody >>~gdownload.vbs
+echo Stream.SaveToFile WScript.Arguments.Item(1), 2 >>~gdownload.vbs
 
 
 set has_user_json=0
@@ -31,7 +34,7 @@ if exist "httpproxy.json" (
 set filename=
 (
     echo 1. Checking GoProxy Version
-    cscript /nologo "%TEMP%\gdownload.vbs" https://github.com/phuslu/goproxy/releases/tag/goproxy ~goproxy_tag.txt
+    cscript /nologo ~gdownload.vbs https://github.com/phuslu/goproxy/releases/tag/goproxy ~goproxy_tag.txt
 ) && (
     for /F "tokens=3 delims=<>" %%I in ('findstr "<strong>goproxy_windows_amd64-r" ~goproxy_tag.txt') do (
         set filename=%%I
@@ -45,14 +48,14 @@ if "%filename%" == "" (
 
 (
     echo 2. Downloading %filename%
-    cscript /nologo "%TEMP%\gdownload.vbs" https://github.com/phuslu/goproxy/releases/download/goproxy/%filename% "~%filename%"
+    cscript /nologo ~gdownload.vbs https://github.com/phuslu/goproxy/releases/download/goproxy/%filename% "~%filename%"
     if not exist "~%filename%" (
         echo Cannot download %filename%
         goto quit
     )
 ) && (
     echo 3. Downloading 7za.exe for extracting
-    cscript /nologo "%TEMP%\gdownload.vbs" https://github.com/phuslu/goproxy/releases/download/assets/7za.exe ~7za.exe
+    cscript /nologo ~gdownload.vbs https://github.com/phuslu/goproxy/releases/download/assets/7za.exe ~7za.exe
     if not exist "~7za.exe" (
         echo Cannot download 7za.exe
         goto quit
@@ -60,15 +63,15 @@ if "%filename%" == "" (
 ) && (
     echo 4. Checking Goproxy program
 :checkgoproxyprogram
-    tasklist /NH /FI "IMAGENAME eq goproxy.exe" | findstr "goproxy.exe" >NUL && (
-        echo %TIME% Please quit GoProxy program.
-        ping -n 2 127.0.0.1 >NUL
-        goto checkgoproxyprogram
+    if exist "goproxy.exe" (
+        tasklist /NH /FI "IMAGENAME eq goproxy.exe" | findstr "goproxy.exe" >NUL && (
+            echo %TIME% Please quit GoProxy program.
+            ping -n 2 127.0.0.1 >NUL
+            goto checkgoproxyprogram
+        )
     )
     echo 5. Extract Goproxy files
     ~7za.exe x -y ~%filename%
-    del /f ~7za.exe ~%filename%
-) && (
     echo 6. Done
 )
 
