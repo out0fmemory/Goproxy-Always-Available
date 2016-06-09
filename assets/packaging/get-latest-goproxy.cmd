02ee65c67c747a0e431017b68c5ac57367284e41
Phus Lu <phuslu@hotmail.com>
2016-06-09 19:28:24 +0800

    refactor get-latest-goproxy.cmd

--- assets/packaging/get-latest-goproxy.cmd
+++ assets/packaging/get-latest-goproxy.cmd
@@ -5,6 +5,18 @@ setlocal enabledelayedexpansion
 
 cd /d "%~dp0"
 
+echo. >"%TEMP%\gdownload.vbs"
+echo Set Http = CreateObject("WinHttp.WinHttpRequest.5.1") >>"%TEMP%\gdownload.vbs"
+echo Set Stream = CreateObject("Adodb.Stream") >>"%TEMP%\gdownload.vbs"
+echo Http.SetTimeouts 30*1000, 30*1000, 30*1000, 120*1000  >>"%TEMP%\gdownload.vbs"
+echo Http.Open "GET", WScript.Arguments.Item(0), False >>"%TEMP%\gdownload.vbs"
+echo Http.Send >>"%TEMP%\gdownload.vbs"
+echo Stream.Type = 1 >>"%TEMP%\gdownload.vbs"
+echo Stream.Open >>"%TEMP%\gdownload.vbs"
+echo Stream.Write Http.ResponseBody >>"%TEMP%\gdownload.vbs"
+echo Stream.SaveToFile WScript.Arguments.Item(1), 2 >>"%TEMP%\gdownload.vbs"
+
+
 set has_user_json=0
 if exist "httpproxy.json" (
     for %%I in (*.user.json) do (
@@ -12,15 +24,14 @@ if exist "httpproxy.json" (
     )
     if "!has_user_json!" == "0" (
         echo Please backup your config as .user.json
-        pause >NUL
-        exit /b -1
+        goto quit
     )
 )
 
 set filename=
 (
     echo 1. Checking GoProxy Version
-    powershell -Command "Invoke-WebRequest https://github.com/phuslu/goproxy/releases/tag/goproxy -OutFile ~goproxy_tag.txt"
+    cscript /nologo "%TEMP%\gdownload.vbs" https://github.com/phuslu/goproxy/releases/tag/goproxy ~goproxy_tag.txt
 ) && (
     for /F "tokens=3 delims=<>" %%I in ('findstr "<strong>goproxy_windows_amd64-r" ~goproxy_tag.txt') do (
         set filename=%%I
@@ -29,21 +40,29 @@ set filename=
 del /f ~goproxy_tag.txt
 if "%filename%" == "" (
     echo Cannot detect goproxy_windows_amd64 version
-    pause >NUL
-    exit /b -1
+    goto quit
 )
 
 (
     echo 2. Downloading %filename%
-    powershell -Command "Invoke-WebRequest https://github.com/phuslu/goproxy/releases/download/goproxy/%filename% -OutFile ~%filename%"
+    cscript /nologo "%TEMP%\gdownload.vbs" https://github.com/phuslu/goproxy/releases/download/goproxy/%filename% "~%filename%"
+    if not exist "~%filename%" (
+        echo Cannot download %filename%
+        goto quit
+    )
+) && (
     echo 3. Downloading 7za.exe for extracting
-    powershell -Command "Invoke-WebRequest https://github.com/phuslu/goproxy/releases/download/assets/7za.exe -OutFile ~7za.exe"
+    cscript /nologo "%TEMP%\gdownload.vbs" https://github.com/phuslu/goproxy/releases/download/assets/7za.exe ~7za.exe
+    if not exist "~7za.exe" (
+        echo Cannot download 7za.exe
+        goto quit
+    )
 ) && (
     echo 4. Checking Goproxy program
 :checkgoproxyprogram
     tasklist /NH /FI "IMAGENAME eq goproxy.exe" | findstr "goproxy.exe" >NUL && (
         echo %TIME% Please quit GoProxy program.
-        ping -n 3 127.0.0.1 >NUL
+        ping -n 2 127.0.0.1 >NUL
         goto checkgoproxyprogram
     )
     echo 5. Extract Goproxy files
@@ -51,7 +70,8 @@ if "%filename%" == "" (
     del /f ~7za.exe ~%filename%
 ) && (
     echo 6. Done
-) || (
-    del /f ~* 2>NUL
 )
-pause >NUL
+
+:quit
+    del /f ~* 2>NUL
+    pause >NUL
