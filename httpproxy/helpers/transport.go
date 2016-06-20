5c7dec04c56a905ab9cc6880e63743c7fd4fac14
Phus Lu <phuslu@hotmail.com>
2016-06-20 21:50:29 +0800

    add cloneRequest to helpers

--- httpproxy/helpers/transport.go
+++ httpproxy/helpers/transport.go
@@ -49,3 +49,17 @@ func FixRequestURL(req *http.Request) {
 		}
 	}
 }
+
+// CloneRequest returns a clone of the provided *http.Request.
+// The clone is a shallow copy of the struct and its Header map.
+func CloneRequest(r *http.Request) *http.Request {
+	// shallow copy of the struct
+	r2 := new(http.Request)
+	*r2 = *r
+	// deep copy of the Header
+	r2.Header = make(http.Header, len(r.Header))
+	for k, s := range r.Header {
+		r2.Header[k] = append([]string(nil), s...)
+	}
+	return r2
+}
