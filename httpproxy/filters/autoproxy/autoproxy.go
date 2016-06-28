956758a49fd4920fdc24f9814bdfe5d2206bb792
Phus Lu <phuslu@hotmail.com>
2016-06-28 20:29:32 +0800

    refine autoproxy index files

--- httpproxy/filters/autoproxy/autoproxy.go
+++ httpproxy/filters/autoproxy/autoproxy.go
@@ -250,11 +250,26 @@ func (f *Filter) IndexFilesRoundTrip(ctx context.Context, req *http.Request) (co
 	<head>
 		<meta charset="UTF-8">
 		<link rel="icon" type="image/x-icon" href="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" />
+		<style>
+		div {width: 200px;margin: 30px;}
+		h2 {font: 400 40px/1.5 Helvetica, Verdana, sans-serif;margin: 0;padding: 0;}
+		ul {list-style-type: none;margin: 0;padding: 0;}
+		li {font: 200 20px/1.5 Helvetica, Verdana, sans-serif;border-bottom: 1px solid #ccc;}
+		li:last-child {border: none;}
+		li a {text-decoration: none;color: #000;-webkit-transition: font-size 0.3s ease, background-color 0.3s ease;
+				-moz-transition: font-size 0.3s ease, background-color 0.3s ease;-o-transition: font-size 0.3s ease, background-color 0.3s ease;
+				-ms-transition: font-size 0.3s ease, background-color 0.3s ease;transition: font-size 0.3s ease, background-color 0.3s ease;
+				display: block;width: 200px;}
+		li a:hover {font-size: 30px;background: #f6f6f6;}
+		</style>
 	</head>
 	<body>
+		<h2>Index of /</h>
+		<div><ul>
 		{{ range $key, $value := .IndexFiles }}
 		   <li><a href="{{ $key }}">{{ $key }}</a></li>
 		{{ end }}
+		</ul></div>
 	</body>
 </html>`
 		t, err := template.New("index").Parse(tpl)
