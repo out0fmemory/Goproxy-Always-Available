b6dd84811906a7b79ce2f49e67de2294eb613110
Phus Lu <phuslu@hotmail.com>
2016-07-03 13:16:30 +0100

    refactor autoproxy filter

---
+++ httpproxy/filters/autoproxy/autoproxyindex.go
@@ -0,0 +1,83 @@
+package autoproxy
+
+import (
+	"bytes"
+	"context"
+	"html/template"
+	"io/ioutil"
+	"mime"
+	"net/http"
+	"path/filepath"
+)
+
+func (f *Filter) IndexFilesRoundTrip(ctx context.Context, req *http.Request) (context.Context, *http.Response, error) {
+	filename := req.URL.Path[1:]
+
+	if filename == "" {
+		const tpl = `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>Index of /</title>
+</head>
+<body>
+<h1>Index of /</h1>
+<pre>Name</pre><hr/>
+<pre>{{ range $key, $value := .IndexFiles }}
+📄 <a href="{{ $key }}">{{ $key }}</a>{{ end }}</pre>
+<hr/><address style="font-size:small;">GoProxy Server</address>
+</body>
+</html>`
+		t, err := template.New("index").Parse(tpl)
+		if err != nil {
+			return ctx, nil, err
+		}
+
+		b := new(bytes.Buffer)
+		err = t.Execute(b, struct{ IndexFiles map[string]struct{} }{f.IndexFiles})
+		if err != nil {
+			return ctx, nil, err
+		}
+
+		return ctx, &http.Response{
+			StatusCode: http.StatusOK,
+			Header: http.Header{
+				"Content-Type": []string{"text/html"},
+			},
+			Request:       req,
+			Close:         true,
+			ContentLength: int64(b.Len()),
+			Body:          ioutil.NopCloser(b),
+		}, nil
+	}
+
+	obj, err := f.Store.GetObject(filename, -1, -1)
+	if err != nil {
+		return ctx, nil, err
+	}
+
+	body := obj.Body()
+	defer body.Close()
+
+	data, err := ioutil.ReadAll(body)
+	if err != nil {
+		return ctx, nil, err
+	}
+
+	contentType := mime.TypeByExtension(filepath.Ext(filename))
+	if contentType == "" {
+		contentType = http.DetectContentType(data)
+	}
+
+	return ctx, &http.Response{
+		StatusCode: http.StatusOK,
+		Header: http.Header{
+			"Content-Type": []string{contentType},
+		},
+		Request:       req,
+		Close:         true,
+		ContentLength: int64(len(data)),
+		Body:          ioutil.NopCloser(bytes.NewReader(data)),
+	}, nil
+}
--- httpproxy/filters/autoproxy/autoproxypac.go
+++ httpproxy/filters/autoproxy/autoproxypac.go
@@ -2,13 +2,260 @@ package autoproxy
 
 import (
 	"bufio"
+	"bytes"
+	"context"
+	"encoding/base64"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
 	"net/url"
+	"os"
 	"regexp"
+	"sort"
 	"strings"
+	"time"
+
+	"github.com/phuslu/glog"
+
+	"../../filters"
 )
 
-func ParseAutoProxy(r io.Reader) ([]string, error) {
+func (f *Filter) ProxyPacRoundTrip(ctx context.Context, req *http.Request) (context.Context, *http.Response, error) {
+	_, port, err := net.SplitHostPort(req.Host)
+	if err != nil {
+		port = "80"
+	}
+
+	if v, ok := f.ProxyPacCache.Get(req.RequestURI); ok {
+		if s, ok := v.(string); ok {
+			s = fixProxyPac(s, req)
+			return ctx, &http.Response{
+				StatusCode:    http.StatusOK,
+				Header:        http.Header{},
+				Request:       req,
+				Close:         true,
+				ContentLength: int64(len(s)),
+				Body:          ioutil.NopCloser(strings.NewReader(s)),
+			}, nil
+		}
+	}
+
+	filename := req.URL.Path[1:]
+
+	buf := new(bytes.Buffer)
+
+	obj, err := f.Store.GetObject(filename, -1, -1)
+	switch {
+	case os.IsNotExist(err):
+		glog.V(2).Infof("AUTOPROXY ProxyPac: generate %#v", filename)
+		s := fmt.Sprintf(`// User-defined FindProxyForURL
+function FindProxyForURL(url, host) {
+    if (shExpMatch(host, '*.google*.*') ||
+       dnsDomainIs(host, '.ggpht.com') ||
+       dnsDomainIs(host, '.gstatic.com') ||
+       host == 'goo.gl') {
+        return 'PROXY localhost:%s';
+    }
+    return 'DIRECT';
+}
+`, port)
+		f.Store.PutObject(filename, http.Header{}, ioutil.NopCloser(bytes.NewBufferString(s)))
+	case err != nil:
+		return ctx, nil, err
+	case obj != nil:
+		if body := obj.Body(); body != nil {
+			body.Close()
+		}
+	}
+
+	if obj, err := f.Store.GetObject(filename, -1, -1); err == nil {
+		body := obj.Body()
+		defer body.Close()
+		if b, err := ioutil.ReadAll(body); err == nil {
+			s := strings.Replace(string(b), "function FindProxyForURL(", "function MyFindProxyForURL(", 1)
+			io.WriteString(buf, s)
+		}
+	}
+
+	if f.GFWListEnabled {
+		object, err := f.Store.GetObject(f.GFWList.Filename, -1, -1)
+		if err != nil {
+			glog.Errorf("GetObject(%#v) error: %v", f.GFWList.Filename, err)
+			return ctx, nil, err
+		}
+
+		rc := object.Body()
+		defer rc.Close()
+		sites, err := parseAutoProxy(rc)
+		if err != nil {
+			glog.Errorf("parseAutoProxy(%#v) error: %v", f.GFWList.Filename, err)
+			return ctx, nil, err
+		}
+
+		sort.Strings(sites)
+
+		io.WriteString(buf, "var sites = {\n")
+		for _, site := range sites {
+			io.WriteString(buf, "\""+site+"\":1,\n")
+		}
+		io.WriteString(buf, "\"google.com\":1\n")
+		io.WriteString(buf, "}\n")
+
+		io.WriteString(buf, `
+function FindProxyForURL(url, host) {
+    if (isPlainHostName(host) ||
+        host.indexOf('127.') == 0 ||
+        host.indexOf('192.168.') == 0 ||
+        host.indexOf('10.') == 0 ||
+        shExpMatch(host, 'localhost.*')) {
+        return 'DIRECT';
+    }
+
+    proxy = MyFindProxyForURL(url, host)
+    if (proxy != "DIRECT") {
+        return proxy
+    }
+
+    var lastPos;
+    do {
+        if (sites.hasOwnProperty(host)) {
+            return 'PROXY GOPROXY_ADDRESS';
+        }
+        lastPos = host.indexOf('.') + 1;
+        host = host.slice(lastPos);
+    } while (lastPos >= 1);
+    return 'DIRECT';
+}`)
+	} else {
+		io.WriteString(buf, `
+function FindProxyForURL(url, host) {
+    if (isPlainHostName(host) ||
+        host.indexOf('127.') == 0 ||
+        host.indexOf('192.168.') == 0 ||
+        host.indexOf('10.') == 0 ||
+        shExpMatch(host, 'localhost.*')) {
+        return 'DIRECT';
+    }
+
+    return MyFindProxyForURL(url, host);
+}`)
+	}
+
+	s := buf.String()
+	f.ProxyPacCache.Set(req.RequestURI, s, time.Now().Add(15*time.Minute))
+
+	s = fixProxyPac(s, req)
+	resp := &http.Response{
+		StatusCode:    http.StatusOK,
+		Header:        http.Header{},
+		Request:       req,
+		Close:         true,
+		ContentLength: int64(len(s)),
+		Body:          ioutil.NopCloser(strings.NewReader(s)),
+	}
+
+	return ctx, resp, nil
+}
+
+func (f *Filter) pacUpdater() {
+	glog.V(2).Infof("start updater for %+v, expiry=%s, duration=%s", f.GFWList.URL.String(), f.GFWList.Expiry, f.GFWList.Duration)
+
+	ticker := time.Tick(f.GFWList.Duration)
+
+	for {
+		select {
+		case <-ticker:
+			glog.V(2).Infof("Begin auto gfwlist(%#v) update...", f.GFWList.URL.String())
+			h, err := f.Store.HeadObject(f.GFWList.Filename)
+			if err != nil {
+				glog.Warningf("stat gfwlist(%#v) err: %v", f.GFWList.Filename, err)
+				continue
+			}
+
+			lm := h.Get("Last-Modified")
+			if lm == "" {
+				glog.Warningf("gfwlist(%#v) header(%#v) does not contains last-modified", f.GFWList.Filename, h)
+				continue
+			}
+
+			modTime, err := time.Parse(f.Store.DateFormat(), lm)
+			if err != nil {
+				glog.Warningf("stat gfwlist(%#v) has parse %#v error: %v", f.GFWList.Filename, lm, err)
+				continue
+			}
+
+			if time.Now().Sub(modTime) < f.GFWList.Expiry {
+				continue
+			}
+		}
+
+		glog.Infof("Downloading %#v", f.GFWList.URL.String())
+
+		req, err := http.NewRequest(http.MethodGet, f.GFWList.URL.String(), nil)
+		if err != nil {
+			glog.Warningf("NewRequest(%#v) error: %v", f.GFWList.URL.String(), err)
+			continue
+		}
+
+		resp, err := f.Transport.RoundTrip(req)
+		if err != nil {
+			glog.Warningf("%T.RoundTrip(%#v) error: %v", f.Transport, f.GFWList.URL.String(), err.Error())
+			continue
+		}
+
+		var r io.Reader = resp.Body
+		switch f.GFWList.Encoding {
+		case "base64":
+			r = base64.NewDecoder(base64.StdEncoding, r)
+		default:
+			break
+		}
+
+		data, err := ioutil.ReadAll(r)
+		if err != nil {
+			glog.Warningf("ioutil.ReadAll(%T) error: %v", r, err)
+			resp.Body.Close()
+			continue
+		}
+
+		err = f.Store.DeleteObject(f.GFWList.Filename)
+		if err != nil {
+			glog.Warningf("%T.DeleteObject(%#v) error: %v", f.Store, f.GFWList.Filename, err)
+			continue
+		}
+
+		err = f.Store.PutObject(f.GFWList.Filename, http.Header{}, ioutil.NopCloser(bytes.NewReader(data)))
+		if err != nil {
+			glog.Warningf("%T.PutObject(%#v) error: %v", f.Store, f.GFWList.Filename, err)
+			continue
+		}
+
+		f.ProxyPacCache.Clear()
+
+		glog.Infof("Update %#v from %#v OK", f.GFWList.Filename, f.GFWList.URL.String())
+		resp.Body.Close()
+	}
+}
+
+func fixProxyPac(s string, req *http.Request) string {
+	s = strings.Replace(s, "GOPROXY_ADDRESS", req.Host, -1)
+
+	ports := make([]string, 0)
+	for _, addr := range []string{req.Host, filters.GetListener(req.Context()).Addr().String()} {
+		_, port, err := net.SplitHostPort(addr)
+		if err != nil {
+			port = "80"
+		}
+		ports = append(ports, port)
+	}
+
+	r := regexp.MustCompile(`PROXY (127.0.0.1|\[::1\]|localhost):(` + strings.Join(ports, "|") + `)`)
+	return r.ReplaceAllString(s, "PROXY "+req.Host)
+}
+
+func parseAutoProxy(r io.Reader) ([]string, error) {
 	scanner := bufio.NewScanner(r)
 
 	sites := make(map[string]struct{}, 0)
