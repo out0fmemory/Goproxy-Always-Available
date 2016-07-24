cf06be215c78f58378f78d8b8bced0ea5ecd720b
Phus Lu <phuslu@hotmail.com>
2016-07-25 00:45:00 +0800

    continue work on get-latest-goproxy.sh

--- assets/packaging/get-latest-goproxy.sh
+++ assets/packaging/get-latest-goproxy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+cd $(python -c "import os; print(os.path.dirname(os.path.realpath('$0')))")
+
 if [ -f "httpproxy.json" ]; then
     if ! ls *.user.json ; then
         echo "Please backup your config as .user.json"
@@ -9,14 +11,23 @@ if [ -f "httpproxy.json" ]; then
     fi
 fi
 
+LOCALVERSION=$(./goproxy -version 2>/dev/null || :)
+echo "0. Local Goproxy version $LOCALVERSION"
+
 echo "1. Checking GoProxy Version"
-FILENAME=$(curl https://github.com/phuslu/goproxy/releases/tag/goproxy | grep -oP '<strong>\Kgoproxy_linux_amd64-r\d+\.tar.xz')
-if [ -z "${FILENAME}" ]; then
+REMOTEVERSION=$(curl https://github.com/phuslu/goproxy/releases/tag/goproxy | grep -oP '<strong>\Kgoproxy_linux_amd64-\Kr\d+')
+if [ -z "${REMOTEVERSION}" ]; then
     echo "Cannot detect goproxy_linux_amd64 version"
     exit -1
 fi
 
+if [ x"$LOCALVERSION" == x"$REMOTEVERSION" ]; then
+	echo "Your GoProxy already update to latest"
+	exit -1
+fi
+
 echo "2. Downloading ${FILENAME}"
-curl -kL https://github.com/phuslu/goproxy/releases/download/goproxy/${FILENAME} | xz -d | tar xvp
+FILENAME=goproxy_linux_amd64-$REMOTEVERSION.tar.xz
+curl -kL https://github.com/phuslu/goproxy/releases/download/goproxy/${FILENAME} | xz -d | tar xvp --strip-components=1
 
 echo "3. Done"
