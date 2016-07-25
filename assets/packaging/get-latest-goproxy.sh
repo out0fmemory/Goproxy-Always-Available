d714ff11d40bbb6a955a8c355d15368945c1a5e9
Phus Lu <phuslu@hotmail.com>
2016-07-25 22:06:24 +0800

    simplify get-latest-goproxy.sh

--- assets/packaging/get-latest-goproxy.sh
+++ assets/packaging/get-latest-goproxy.sh
@@ -17,52 +17,42 @@ case $(uname -s)/$(uname -m) in
 	Linux/x86_64 )
 		FILENAME_PREFIX=goproxy_linux_amd64-
 		FILENAME_SUFFIX=.tar.xz
-		FILENAME_DISTCMD="xz -d"
 		;;
 	Linux/i686|Linux/i386 )
 		FILENAME_PREFIX=goproxy_linux_386-
 		FILENAME_SUFFIX=.tar.xz
-		FILENAME_DISTCMD="xz -d"
 		;;
 	Linux/armv7l|Linux/armv8 )
 		FILENAME_PREFIX=goproxy_linux_arm64-
 		FILENAME_SUFFIX=.tar.xz
-		FILENAME_DISTCMD="xz -d"
 		;;
 	Linux/arm* )
 		FILENAME_PREFIX=goproxy_linux_arm-
 		FILENAME_SUFFIX=.tar.xz
-		FILENAME_DISTCMD="xz -d"
 		;;
 	Linux/mips64el )
 		FILENAME_PREFIX=goproxy_linux_mips64le-
 		FILENAME_SUFFIX=.tar.xz
-		FILENAME_DISTCMD="xz -d"
 		;;
 	Linux/mips64 )
 		FILENAME_PREFIX=goproxy_linux_mips64-
 		FILENAME_SUFFIX=.tar.xz
-		FILENAME_DISTCMD="xz -d"
 		;;
 	FreeBSD/x86_64 )
 		FILENAME_PREFIX=goproxy_freebsd_amd64-
 		FILENAME_SUFFIX=.tar.bz2
-		FILENAME_DISTCMD="xz -d"
 		;;
 	FreeBSD/i686|FreeBSD/i386 )
 		FILENAME_PREFIX=goproxy_freebsd_386-
 		FILENAME_SUFFIX=.tar.bz2
-		FILENAME_DISTCMD="xz -d"
 		;;
 	Darwin/x86_64 )
 		FILENAME_PREFIX=goproxy_macos_amd64-
 		FILENAME_SUFFIX=.tar.bz2
-		FILENAME_DISTCMD="bzip2 -d"
 		;;
 	Darwin/i686|Darwin/i386 )
 		FILENAME_PREFIX=goproxy_macos_386-
 		FILENAME_SUFFIX=.tar.bz2
-		FILENAME_DISTCMD="bzip2 -d"
 		;;
 	* )
 		echo "Unsupported platform: $(uname -a)"
@@ -87,6 +77,6 @@ fi
 
 echo "2. Downloading ${FILENAME}"
 FILENAME=${FILENAME_PREFIX}${REMOTEVERSION}${FILENAME_SUFFIX}
-curl -kL https://github.com/phuslu/goproxy/releases/download/goproxy/${FILENAME} | ${FILENAME_DISTCMD} | tar xvp --strip-components=1
+curl -kL https://github.com/phuslu/goproxy/releases/download/goproxy/${FILENAME} | ([[ "${FILENAME_SUFFIX}" == *.xz ]] && xz -d || bzip2 -d)  | tar xvp --strip-components=1
 
 echo "3. Done"
