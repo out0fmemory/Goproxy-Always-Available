426426cf2fda96d6893ad9dd43e65528bc90a2bb
Phus Lu <phuslu@hotmail.com>
2016-08-01 11:16:25 -0500

    Update autoproxypac.go

--- httpproxy/filters/autoproxy/autoproxypac.go
+++ httpproxy/filters/autoproxy/autoproxypac.go
@@ -57,7 +57,7 @@ function FindProxyForURL(url, host) {
         isInNet(host, "10.0.0.0", "255.0.0.0") ||
         isInNet(host, "172.16.0.0",  "255.240.0.0") ||
         isInNet(host, "192.168.0.0",  "255.255.0.0") ||
-        isInNet(host, "127.0.0.0", "255.255.255.0")) ||
+        isInNet(host, "127.0.0.0", "255.255.255.0") ||
         shExpMatch(host, "*.local") ||
         shExpMatch(host, 'localhost.*')) {
         return 'DIRECT';
