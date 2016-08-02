fed2271025acf6d85bc35f400c2b6587668984f5
Phus Lu <phuslu@hotmail.com>
2016-08-02 21:24:14 +0800

    add 169.254.x.x to proxy.pac

--- httpproxy/filters/autoproxy/autoproxypac.go
+++ httpproxy/filters/autoproxy/autoproxypac.go
@@ -55,8 +55,9 @@ func (f *Filter) ProxyPacRoundTrip(ctx context.Context, req *http.Request) (cont
 function FindProxyForURL(url, host) {
     if (isPlainHostName(host) ||
         isInNet(host, "10.0.0.0", "255.0.0.0") ||
-        isInNet(host, "172.16.0.0",  "255.240.0.0") ||
-        isInNet(host, "192.168.0.0",  "255.255.0.0") ||
+        isInNet(host, "172.16.0.0", "255.240.0.0") ||
+        isInNet(host, "169.254.0.0", "255.255.0.0") ||
+        isInNet(host, "192.168.0.0", "255.255.0.0") ||
         isInNet(host, "127.0.0.0", "255.255.255.0") ||
         shExpMatch(host, "*.local") ||
         shExpMatch(host, 'localhost.*')) {
