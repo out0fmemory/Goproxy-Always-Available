939710ac1bdfe524fe42542eec47f4b0dc0f49aa
Phus Lu <phuslu@hotmail.com>
2016-08-09 09:51:48 +0800

    fix race condition, agian

--- httpproxy/filters/filters.go
+++ httpproxy/filters/filters.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 type Filter interface {
@@ -32,11 +33,13 @@ type RegisteredFilter struct {
 var (
 	registeredFilters map[string]*RegisteredFilter
 	newedFilters      map[string]Filter
+	muFilters         map[string]*sync.Mutex
 )
 
 func init() {
 	registeredFilters = make(map[string]*RegisteredFilter)
 	newedFilters = make(map[string]Filter)
+	muFilters = make(map[string]*sync.Mutex)
 }
 
 // Register a Filter
@@ -46,11 +49,15 @@ func Register(name string, registeredFilter *RegisteredFilter) error {
 	}
 
 	registeredFilters[name] = registeredFilter
+	muFilters[name] = new(sync.Mutex)
 	return nil
 }
 
 // GetFilter try get a existing Filter of type "name", otherwise create new one
 func GetFilter(name string) (Filter, error) {
+	muFilters[name].Lock()
+	defer muFilters[name].Unlock()
+
 	if f, ok := newedFilters[name]; ok {
 		return f, nil
 	}
