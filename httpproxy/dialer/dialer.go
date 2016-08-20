9a07f42a08bec9e51c66008d720eeaf60ba2efd3
Phus Lu <phuslu@hotmail.com>
2016-08-20 19:12:27 +0800

    refactor dialer to fix bad dns record

--- httpproxy/dialer/dialer.go
+++ httpproxy/dialer/dialer.go
@@ -40,95 +40,66 @@ func (d *Dialer) Dial(network, address string) (conn net.Conn, err error) {
 			} else {
 				if host, port, err := net.SplitHostPort(address); err == nil {
 					if ips, err := net.LookupIP(host); err == nil && len(ips) > 0 {
-						var ip string
-						if d.BlackList != nil {
-							for _, ip1 := range ips {
-								if _, ok := d.BlackList.Get(ip1.String()); !ok {
-									ip = ip1.String()
-								}
-							}
-						} else {
-							ip = ips[0].String()
-						}
-
-						if ip == "" {
-							return nil, net.InvalidAddrError(fmt.Sprintf("Invaid DNS Record: %s(%+v)", host, ips))
-						}
-
-						addr := net.JoinHostPort(ip, port)
-						expiry := d.DNSCacheExpiry
-						if expiry == 0 {
-							expiry = DefaultDNSCacheExpiry
-						}
-						d.DNSCache.Set(address, addr, time.Now().Add(expiry))
-						glog.V(3).Infof("direct Dial cache dns %#v=%#v", address, addr)
-						address = addr
+						return d.dialMulti(network, address, ips, port)
 					}
 				}
 			}
 		}
-	default:
-		break
 	}
 
-	if d.Level <= 1 {
-		retry := d.RetryTimes
-		if retry == 0 {
-			retry = DefaultRetryTimes
-		}
+	return d.Dialer.Dial(network, address)
+}
 
-		for i := 0; i < retry; i++ {
-			conn, err = d.Dialer.Dial(network, address)
-			if err == nil || i == retry-1 {
-				break
+func (d *Dialer) dialMulti(network, address string, ips []net.IP, port string) (conn net.Conn, err error) {
+	if d.Level <= 1 || len(ips) == 1 {
+		for i, ip := range ips {
+			ipstr := ip.String()
+			if d.BlackList != nil {
+				if _, ok := d.BlackList.Get(ipstr); ok {
+					if i < len(ips)-1 {
+						continue
+					} else {
+						return nil, net.InvalidAddrError(fmt.Sprintf("Invaid DNS Record: %s(%+v)", address, ipstr))
+					}
+				}
 			}
-			if ne, ok := err.(interface {
-				Timeout() bool
-			}); ok && ne.Timeout() {
-				d.BlackList.Set(address, struct{}{}, time.Now().Add(5*time.Hour))
-			}
-			retryDelay := d.RetryDelay
-			if retryDelay == 0 {
-				retryDelay = DefaultRetryDelay
-			}
-			time.Sleep(retryDelay)
+			addr := net.JoinHostPort(ipstr, port)
+			return d.Dialer.Dial(network, addr)
 		}
-		return conn, err
 	} else {
 		type racer struct {
 			c net.Conn
 			e error
 		}
 
-		lane := make(chan racer, d.Level)
-		retry := (d.RetryTimes + d.Level - 1) / d.Level
-		for i := 0; i < retry; i++ {
-			for j := 0; j < d.Level; j++ {
-				go func(addr string, c chan<- racer) {
-					conn, err := d.Dialer.Dial(network, addr)
-					lane <- racer{conn, err}
-				}(address, lane)
-			}
+		level := len(ips)
+		if level > d.Level {
+			level = d.Level
+			ips = ips[:level]
+		}
 
-			var r racer
-			for k := 0; k < d.Level; k++ {
-				r = <-lane
-				if r.e == nil {
-					go func(count int) {
-						var r1 racer
-						for ; count > 0; count-- {
-							r1 = <-lane
-							if r1.c != nil {
-								r1.c.Close()
-							}
+		lane := make(chan racer, level)
+		for i := 0; i < level; i++ {
+			go func(addr string, c chan<- racer) {
+				conn, err := d.Dialer.Dial(network, addr)
+				lane <- racer{conn, err}
+			}(net.JoinHostPort(ips[i].String(), port), lane)
+		}
+
+		var r racer
+		for j := 0; j < level; j++ {
+			r = <-lane
+			if r.e == nil {
+				go func(count int) {
+					var r1 racer
+					for ; count > 0; count-- {
+						r1 = <-lane
+						if r1.c != nil {
+							r1.c.Close()
 						}
-					}(d.Level - 1 - k)
-					return r.c, nil
-				}
-			}
-
-			if i == retry-1 {
-				return nil, r.e
+					}
+				}(level - 1 - j)
+				return r.c, nil
 			}
 		}
 	}
