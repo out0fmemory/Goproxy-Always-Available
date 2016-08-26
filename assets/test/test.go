3a378e2ac04193eed43ab45428e0901fa9bdc143
Phus Lu <phuslu@hotmail.com>
2016-08-26 11:47:03 +0800

    update test.go

--- assets/test/test.go
+++ assets/test/test.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
-	fmt.Printf("%#v, err=%#v\n", "foo", nil)
+	v, err := net.LookupIP("localhost")
+	fmt.Printf("%+v, err=%#v\n", v, err)
 }
