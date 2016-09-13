1f5742ad96bfbaee21bbdf25b7fd43fd66595ff1
Phus Lu <phuslu@hotmail.com>
2016-09-14 00:47:54 +0800

    upgrade to go 1.7.1 in ci.sh

--- assets/build/ci.sh
+++ assets/build/ci.sh
@@ -55,7 +55,7 @@ function init_github() {
 function build_go() {
 	pushd ${WORKING_DIR}
 
-	curl -k https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz | tar xz
+	curl -k https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz | tar xz
 	mv go goroot_bootstrap
 
 	git clone --branch ${GOBRANCH} https://github.com/phuslu/go
