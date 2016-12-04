e05dff8eac03bcea26ab8b7539e4ac7bd13361e2
Phus Lu <phuslu@hotmail.com>
2016-12-04 15:25:31 +0800

    switch to io.CopyBuffer

--- httpproxy/helpers/io.go
+++ httpproxy/helpers/io.go
@@ -19,40 +19,8 @@ var (
 )
 
 func IOCopy(dst io.Writer, src io.Reader) (written int64, err error) {
-	// If the reader has a WriteTo method, use it to do the copy.
-	// Avoids an allocation and a copy.
-	if wt, ok := src.(io.WriterTo); ok {
-		return wt.WriteTo(dst)
-	}
-	// Similarly, if the writer has a ReadFrom method, use it to do the copy.
-	if rt, ok := dst.(io.ReaderFrom); ok {
-		return rt.ReadFrom(src)
-	}
 	buf := bufpool.Get().([]byte)
-	defer bufpool.Put(buf)
-	for {
-		nr, er := src.Read(buf)
-		if nr > 0 {
-			nw, ew := dst.Write(buf[0:nr])
-			if nw > 0 {
-				written += int64(nw)
-			}
-			if ew != nil {
-				err = ew
-				break
-			}
-			if nr != nw {
-				err = io.ErrShortWrite
-				break
-			}
-		}
-		if er == io.EOF {
-			break
-		}
-		if er != nil {
-			err = er
-			break
-		}
-	}
+	written, err = io.CopyBuffer(dst, src, buf)
+	bufpool.Put(buf)
 	return written, err
 }
