d71cb3e1dffa9495aac945fbddc5d135a8b1adf5
Phus Lu <phuslu@hotmail.com>
2017-01-24 22:14:15 +0800

    add more goproxy-vps osarchs

--- assets/build/ci.sh
+++ assets/build/ci.sh
@@ -257,7 +257,7 @@ function build_repo_ex() {
 	git clone --branch ${GOBRANCH} https://github.com/phuslu/goproxy $GOPATH/src/github.com/phuslu/goproxy
 	awk 'match($1, /"((github\.com|golang\.org|gopkg\.in)\/.+)"/) {if (!seen[$1]++) {gsub("\"", "", $1); print $1}}' $(find . -name "*.go") | xargs -n1 -i go get -u -v {}
 
-	for OSARCH in linux/amd64 linux/arm64 linux/arm
+	for OSARCH in linux/amd64 linux/386 linux/arm64 linux/arm
 	do
 		rm -rf goproxy-vps
 		make GOOS=${OSARCH%/*} GOARCH=${OSARCH#*/}
