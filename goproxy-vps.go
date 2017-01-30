761bd3f5b9b39a9e07ea8667f3d09dd7df361c2e
Phus Lu <phuslu@hotmail.com>
2017-01-30 22:44:25 +0800

    bugfix for http frontend
    Signed-off-by: Phus Lu <phuslu@hotmail.com>

--- goproxy-vps.go
+++ goproxy-vps.go
@@ -118,6 +118,170 @@ func (p *SimplePAM) Authenticate(username, password string) error {
 	return nil
 }
 
+type HTTPHandler struct {
+	Dial func(network, address string) (net.Conn, error)
+	*http.Transport
+	*SimplePAM
+}
+
+func (h *HTTPHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	var err error
+
+	var paramsPreifx string = http.CanonicalHeaderKey("X-UrlFetch-")
+	params := http.Header{}
+	for key, values := range req.Header {
+		if strings.HasPrefix(key, paramsPreifx) {
+			params[key] = values
+		}
+	}
+
+	for key := range params {
+		req.Header.Del(key)
+	}
+
+	if h.SimplePAM != nil {
+		auth := req.Header.Get("Proxy-Authorization")
+		if auth == "" {
+			h.ProxyAuthorizationReqiured(rw, req)
+			return
+		}
+
+		parts := strings.SplitN(auth, " ", 2)
+		if len(parts) == 2 {
+			switch parts[0] {
+			case "Basic":
+				if userpass, err := base64.StdEncoding.DecodeString(parts[1]); err == nil {
+					parts := strings.Split(string(userpass), ":")
+					username := parts[0]
+					password := parts[1]
+
+					if err := h.SimplePAM.Authenticate(username, password); err != nil {
+						http.Error(rw, "403 Forbidden", http.StatusForbidden)
+					}
+				}
+			default:
+				glog.Errorf("Unrecognized auth type: %#v", parts[0])
+				http.Error(rw, "403 Forbidden", http.StatusForbidden)
+				return
+			}
+		}
+
+		req.Header.Del("Proxy-Authorization")
+	}
+
+	if req.Method == http.MethodConnect {
+		host, port, err := net.SplitHostPort(req.Host)
+		if err != nil {
+			host = req.Host
+			port = "443"
+		}
+
+		glog.Infof("%s \"%s %s:%s %s\" - -", req.RemoteAddr, req.Method, host, port, req.Proto)
+
+		dial := h.Dial
+		if dial == nil {
+			dial = h.Transport.Dial
+		}
+
+		conn, err := dial("tcp", net.JoinHostPort(host, port))
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusBadGateway)
+			return
+		}
+
+		flusher, ok := rw.(http.Flusher)
+		if !ok {
+			http.Error(rw, fmt.Sprintf("%#v is not http.Flusher", rw), http.StatusBadGateway)
+			return
+		}
+
+		rw.WriteHeader(http.StatusOK)
+		flusher.Flush()
+
+		hijacker, ok := rw.(http.Hijacker)
+		if !ok {
+			http.Error(rw, fmt.Sprintf("%#v is not http.Hijacker", rw), http.StatusBadGateway)
+			return
+		}
+		lconn, _, err := hijacker.Hijack()
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusBadGateway)
+			return
+		}
+		defer lconn.Close()
+		defer conn.Close()
+
+		go helpers.IOCopy(conn, lconn)
+		helpers.IOCopy(lconn, conn)
+
+		return
+	}
+
+	if req.Host == "" {
+		http.Error(rw, "400 Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	if req.URL.Host == "" {
+		req.URL.Host = req.Host
+	}
+
+	if req.ContentLength == 0 {
+		io.Copy(ioutil.Discard, req.Body)
+		req.Body.Close()
+		req.Body = nil
+	}
+
+	glog.Infof("%s \"%s %s %s\" - -", req.RemoteAddr, req.Method, req.URL.String(), req.Proto)
+
+	if req.URL.Scheme == "" {
+		req.URL.Scheme = "http"
+	}
+
+	resp, err := h.Transport.RoundTrip(req)
+	if err != nil {
+		msg := err.Error()
+		if strings.HasPrefix(msg, "Invaid DNS Record: ") {
+			http.Error(rw, "403 Forbidden", http.StatusForbidden)
+		} else {
+			http.Error(rw, err.Error(), http.StatusBadGateway)
+		}
+		return
+	}
+
+	for key, values := range resp.Header {
+		for _, value := range values {
+			rw.Header().Add(key, value)
+		}
+	}
+	rw.WriteHeader(resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	var r io.Reader = resp.Body
+	helpers.IOCopy(rw, r)
+}
+
+func (h *HTTPHandler) ProxyAuthorizationReqiured(rw http.ResponseWriter, req *http.Request) {
+	data := "Proxy Authentication Required"
+	resp := &http.Response{
+		StatusCode: http.StatusProxyAuthRequired,
+		Header: http.Header{
+			"Proxy-Authenticate": []string{"Basic realm=\"Proxy Authentication Required\""},
+		},
+		Request:       req,
+		ContentLength: int64(len(data)),
+		Body:          ioutil.NopCloser(strings.NewReader(data)),
+	}
+	for key, values := range resp.Header {
+		for _, value := range values {
+			rw.Header().Add(key, value)
+		}
+	}
+	rw.WriteHeader(resp.StatusCode)
+	helpers.IOCopy(rw, resp.Body)
+}
+
 type HTTP2Handler struct {
 	Fallback     *url.URL
 	DisableProxy bool
@@ -593,7 +757,7 @@ func main() {
 			glog.Fatalf("Listen(%s) error: %s", addr, err)
 		}
 
-		handler := &HTTP2Handler{
+		handler := &HTTPHandler{
 			Transport: transport,
 		}
 
@@ -634,7 +798,7 @@ func main() {
 		}
 
 		srv := &http.Server{
-			Handler: h,
+			Handler: handler,
 		}
 
 		glog.Infof("goproxy-vps %s ListenAndServe on %s\n", version, ln.Addr().String())
