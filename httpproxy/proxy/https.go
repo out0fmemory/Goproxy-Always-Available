777933f2347ed1288baa0214e6ee15f3c9b8f195
Phus Lu <phuslu@hotmail.com>
2017-04-26 19:39:42 +0800

    switch to bogo in proxy/https.go

--- httpproxy/proxy/https.go
+++ httpproxy/proxy/https.go
@@ -7,7 +7,6 @@ package proxy
 import (
 	"bufio"
 	"bytes"
-	"crypto/tls"
 	"encoding/base64"
 	"errors"
 	"fmt"
@@ -18,6 +17,7 @@ import (
 	"time"
 
 	"github.com/cloudflare/golibs/lrucache"
+	tls "github.com/google/boringssl/ssl/test/runner"
 )
 
 func HTTPS(network, addr string, auth *Auth, forward Dialer, resolver Resolver) (Dialer, error) {
@@ -81,10 +81,10 @@ func (h *https) Dial(network, addr string) (net.Conn, error) {
 		config = &tls.Config{
 			MinVersion:         tls.VersionTLS10,
 			MaxVersion:         tls.VersionTLS13,
-			Max0RTTDataSize:    100 * 1024,
 			InsecureSkipVerify: true,
 			ServerName:         h.hostname,
 			ClientSessionCache: tls.NewLRUClientSessionCache(1024),
+			// Max0RTTDataSize:    100 * 1024,
 		}
 		h.cache.Set(h.addr, config, time.Now().Add(2*time.Hour))
 	}
