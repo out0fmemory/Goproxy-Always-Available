0cc04c16ea8b7aa28909c6c1247e08b6767ee99d
Phus Lu <phuslu@hotmail.com>
2017-06-08 23:26:48 +0800

    add SetFlagsFromEnv

--- httpproxy/helpers/os.go
+++ httpproxy/helpers/os.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -22,4 +23,37 @@ func SetFlagsIfAbsent(m map[string]string) {
 			flag.Set(key, value)
 		}
 	}
+}
+
+// copy/paste from https://github.com/jamiealquiza/envy/
+// Parse takes a string p that is used
+// as the environment variable prefix
+// for each flag configured.
+func SetFlagsFromEnv(prefix string) {
+	// Build a map of explicitly set flags.
+	set := map[string]bool{}
+	flag.CommandLine.Visit(func(f *flag.Flag) {
+		set[f.Name] = true
+	})
+
+	flag.CommandLine.VisitAll(func(f *flag.Flag) {
+		// Create an env var name
+		// based on the supplied prefix.
+		envVar := fmt.Sprintf("%s_%s", prefix, strings.ToUpper(f.Name))
+		envVar = strings.Replace(envVar, "-", "_", -1)
+
+		// Update the Flag.Value if the
+		// env var is non "".
+		if val := os.Getenv(envVar); val != "" {
+			// Update the value if it hasn't
+			// already been set.
+			if defined := set[f.Name]; !defined {
+				flag.CommandLine.Set(f.Name, val)
+			}
+		}
+
+		// Append the env var to the
+		// Flag.Usage field.
+		f.Usage = fmt.Sprintf("%s [%s]", f.Usage, envVar)
+	})
 }
