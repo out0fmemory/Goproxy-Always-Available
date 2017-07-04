d3b92f7e43ddb8fd8d326ab109a6d4ed50e7c203
Phus Lu <phuslu@hotmail.com>
2017-07-04 14:26:02 +0800

    enable dead probe when quic enabled

--- httpproxy/filters/gae/gae.go
+++ httpproxy/filters/gae/gae.go
@@ -352,7 +352,7 @@ func NewFilter(config *Config) (filters.Filter, error) {
 		}
 	}
 
-	if config.EnableDeadProbe && !config.Transport.Proxy.Enabled && !config.EnableQuic {
+	if config.EnableDeadProbe && !config.Transport.Proxy.Enabled {
 		go func() {
 			probe := func() {
 				c, err := net.DialTimeout("tcp", net.JoinHostPort(config.DNSServers[0], "53"), 300*time.Millisecond)
