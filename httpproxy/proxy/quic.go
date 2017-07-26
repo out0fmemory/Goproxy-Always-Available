8d612e702225373d08ef0cb6792976c1a2fdb431
Phus Lu <phuslu@hotmail.com>
2017-07-26 20:16:59 +0800

    switch h2quic from quic-conn

--- httpproxy/proxy/quic.go
+++ httpproxy/proxy/quic.go
@@ -5,21 +5,31 @@
 package proxy
 
 import (
-	"bufio"
-	"bytes"
 	"crypto/tls"
-	"encoding/base64"
 	"errors"
-	"fmt"
-	"io"
 	"net"
 	"net/http"
-	"strconv"
-	"time"
+	"net/url"
 
-	"github.com/cloudflare/golibs/lrucache"
-	"github.com/phuslu/quic-conn"
+	quic "github.com/phuslu/quic-go"
+	"github.com/phuslu/quic-go/h2quic"
 )
+
+type QuicConn struct {
+	quic.Stream
+	Source net.Addr
+	Addr   net.Addr
+}
+
+func (c *QuicConn) LocalAddr() net.Addr {
+	return c.Source
+}
+
+func (c *QuicConn) RemoteAddr() net.Addr {
+	return c.Addr
+}
+
+var _ net.Conn = &QuicConn{}
 
 func QUIC(network, addr string, auth *Auth, forward Dialer, resolver Resolver) (Dialer, error) {
 	var hostname string
@@ -31,13 +41,18 @@ func QUIC(network, addr string, auth *Auth, forward Dialer, resolver Resolver) (
 		addr = net.JoinHostPort(addr, "443")
 	}
 
-	s := &quic{
+	s := &Quic{
 		network:  network,
 		addr:     addr,
 		hostname: hostname,
 		forward:  forward,
 		resolver: resolver,
-		cache:    lrucache.NewLRUCache(128),
+		transport: &h2quic.RoundTripper{
+			DisableCompression: true,
+			DialAddr: func(address string, tlsConfig *tls.Config, cfg *quic.Config) (quic.Session, error) {
+				return quic.DialAddr(addr, tlsConfig, cfg)
+			},
+		},
 	}
 	if auth != nil {
 		s.user = auth.User
@@ -47,104 +62,34 @@ func QUIC(network, addr string, auth *Auth, forward Dialer, resolver Resolver) (
 	return s, nil
 }
 
-type quic struct {
+type Quic struct {
 	user, password string
 	network, addr  string
 	hostname       string
 	forward        Dialer
 	resolver       Resolver
-	cache          lrucache.Cache
+	transport      *h2quic.RoundTripper
 }
 
 // Dial connects to the address addr on the network net via the HTTPS proxy.
-func (h *quic) Dial(network, addr string) (net.Conn, error) {
+func (h *Quic) Dial(network, addr string) (net.Conn, error) {
 	switch network {
 	case "tcp", "tcp6", "tcp4":
 	default:
 		return nil, errors.New("proxy: no support for QUIC proxy connections of type " + network)
 	}
 
-	var config *tls.Config
-	if v, ok := h.cache.GetNotStale(h.addr); ok {
-		config = v.(*tls.Config)
-	} else {
-		config = &tls.Config{
-			MinVersion:         tls.VersionTLS10,
-			MaxVersion:         tls.VersionTLS13,
-			InsecureSkipVerify: true,
-			ServerName:         h.hostname,
-			ClientSessionCache: tls.NewLRUClientSessionCache(1024),
-		}
-		h.cache.Set(h.addr, config, time.Now().Add(2*time.Hour))
+	req := &http.Request{
+		Method: http.MethodConnect,
+		Host:   addr,
+		Header: http.Header{},
+		URL: &url.URL{
+			Scheme: "https",
+			Host:   addr,
+		},
 	}
 
-	conn, err := quicconn.Dial(h.addr, config)
-	if err != nil {
-		return nil, err
-	}
-	closeConn := &conn
-	defer func() {
-		if closeConn != nil {
-			(*closeConn).Close()
-		}
-	}()
-
-	host, portStr, err := net.SplitHostPort(addr)
-	if err != nil {
-		return nil, err
-	}
-
-	port, err := strconv.Atoi(portStr)
-	if err != nil {
-		return nil, errors.New("proxy: failed to parse port number: " + portStr)
-	}
-	if port < 1 || port > 0xffff {
-		return nil, errors.New("proxy: port number out of range: " + portStr)
-	}
-
-	if h.resolver != nil {
-		hosts, err := h.resolver.LookupHost(host)
-		if err == nil && len(hosts) > 0 {
-			host = hosts[0]
-		}
-	}
-
-	b := bufPool.Get().(*bytes.Buffer)
-	b.Reset()
-
-	fmt.Fprintf(b, "CONNECT %s:%s HTTP/1.1\r\nHost: %s:%s\r\n", host, portStr, host, portStr)
-	if h.user != "" {
-		fmt.Fprintf(b, "Proxy-Authorization: Basic %s\r\n", base64.StdEncoding.EncodeToString([]byte(h.user+":"+h.password)))
-	}
-	io.WriteString(b, "\r\n")
-
-	bb := b.Bytes()
-	bufPool.Put(b)
-
-	if _, err := conn.Write(bb); err != nil {
-		return nil, errors.New("proxy: failed to write greeting to HTTP proxy at " + h.addr + ": " + err.Error())
-	}
-
-	buf := make([]byte, 2048)
-	b0 := buf
-	total := 0
-
-	for {
-		n, err := conn.Read(buf)
-		if err != nil {
-			return nil, err
-		}
-		total += n
-		buf = buf[n:]
-
-		if i := bytes.Index(b0, CRLFCRLF); i > 0 {
-			conn = &preReaderConn{conn, b0[i+4 : total]}
-			b0 = b0[:i+4]
-			break
-		}
-	}
-
-	resp, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(b0)), nil)
+	resp, err := h.transport.RoundTrip(req)
 	if err != nil {
 		return nil, err
 	}
@@ -153,6 +98,9 @@ func (h *quic) Dial(network, addr string) (net.Conn, error) {
 		return nil, errors.New("proxy: failed to read greeting from HTTP proxy at " + h.addr + ": " + resp.Status)
 	}
 
-	closeConn = nil
+	conn := &QuicConn{
+		Stream: resp.Body.(quic.Stream),
+	}
+
 	return conn, nil
 }
