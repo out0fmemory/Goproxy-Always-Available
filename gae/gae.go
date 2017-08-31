443656bf52e41b049e7be1403f249b81c266573d
Phus Lu <phuslu@hotmail.com>
2017-09-01 02:18:45 +0800

    fix brotli only site

--- gae/gae.go
+++ gae/gae.go
@@ -296,6 +296,10 @@ func handler(rw http.ResponseWriter, r *http.Request) {
 		resp.Header.Set("Content-Length", strconv.FormatInt(resp.ContentLength, 10))
 	}
 
+	if resp.Header.Get("Content-Encoding") == "br" && req.Header.Get("Accept-Encoding") == "" {
+		resp.Header.Del("Content-Encoding")
+	}
+
 	var chunked bool
 	if resp.Header.Get("Content-Encoding") == "" && IsTextContentType(resp.Header.Get("Content-Type")) {
 		content := reflect.ValueOf(resp.Body).Elem().FieldByName("content").Bytes()
